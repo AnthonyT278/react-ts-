f95b847b34fdb92fc7e7a63261208c9ecec7869c
OpenLearn <tmoropodi@gmail.com>
2024-11-26 18:14:44 +0200

    New changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,11 @@
 
 export default function Home () {
     const a:string = "dont try me 😉";
+    const name:string = "Khaya"
     return (
         <div>
             <h1>Awe Bra 😂😁 {a}</h1>
+             <p>Hey {name}</p> 
         </div>
     )
 }
